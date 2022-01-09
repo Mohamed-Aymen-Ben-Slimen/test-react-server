7e17bb180ce303ab085d69cb41bfbb2e068d7560
Mohamed-Aymen-Ben-Slimen <50459819+Mohamed-Aymen-Ben-Slimen@users.noreply.github.com>
2022-01-09 19:48:01 +0100

    handle request error

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-const projects = [];
+let projects = [];
 
-/* Get projects. */
+/* Get get projects. */
 router.get('/', function(req, res, next) {
   res.status(200).json({
     projects: projects
@@ -14,18 +14,72 @@ router.get('/', function(req, res, next) {
 router.post('/', function(req, res, next) {
   const {project} = req.body;
 
-  if (project) {
+  if (!project) {
+    res.status(400).json({
+      Status: 400,
+      Message: 'Project is not added'
+    });
+  } else {
     projects.push(project);
     res.status(200).json({
       Status: 200,
       Message: 'Project successfully added'
     });
-  } else {
+  }
+});
+
+/* PATCH update project. */
+router.patch('/:projectId', function(req, res, next) {
+  const {projectId} = req.params;
+  const {project} = req.body;
+
+  if (!project || !projectId) {
     res.status(400).json({
       Status: 400,
-      Message: 'Project is not added'
+      Message: 'Project is not updated'
+    });
+  } else if (projectId < 0 || projectId > projects.length) {
+    res.status(400).json({
+      Status: 400,
+      Message: 'Project does not exist'
+    });
+  }else {
+    projects = projects.map( (p, index) => {
+      if (index === projectId) {
+        return project;
+      }
+      return p;
+    });
+    res.status(200).json({
+      Status: 200,
+      Message: 'Project successfully updated',
+      Project: project
     });
   }
+});
+
+/* DELETE delete project. */
+router.delete('/:projectId', function(req, res, next) {
+  const {projectId} = req.params;
+
+  if (!projectId) {
+    res.status(400).json({
+      Status: 400,
+      Message: 'ProjectId is not presented'
+    });
+  } else if (projectId < 0 || projectId > projects.length) {
+    res.status(400).json({
+      Status: 400,
+      Message: 'Project does not exist'
+    });
+  } else {
+    projects = projects.filter( (p, index) => index !== projectId);
+    res.status(200).json({
+      Status: 200,
+      Message: 'Project successfully deleted',
+    });
+  }
+
 });
 
 module.exports = router;
